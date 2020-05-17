0a8da8741d3e3aa10ddd981cf26841dee6225f16
Seungjae Ryan Lee <seungjaeryanlee@gmail.com>
2020-05-17 15:21:05 -0400

    Generate info.csv in preprocessing script

--- preprocess.py
+++ preprocess.py
@@ -12,8 +12,9 @@ from tqdm import tqdm
 
 
 def preprocess(raw_data_dir="data/raw/", clean_data_dir="data/clean/"):
+    data_infos = []
     print(f"Loading raw data from '{raw_data_dir}' to and saving clean data to '{clean_data_dir}'...")
-    for filename in tqdm(glob.glob(f"{raw_data_dir}/I*.csv")):
+    for filename in tqdm(sorted(glob.glob(f"{raw_data_dir}/I*.csv"))):
         p = re.search("(I|V)\((.*)_(.*)_(.*)\)", filename)
         wave_type = p.group(1)
         wave_shape = p.group(2)
@@ -23,8 +24,9 @@ def preprocess(raw_data_dir="data/raw/", clean_data_dir="data/clean/"):
         # Load CSV pair
         mat_i = np.loadtxt(open(f"{raw_data_dir}/I({wave_shape}_{wave_freq}_{wave_cond}).csv", "rb"), delimiter=",")
         mat_v = np.loadtxt(open(f"{raw_data_dir}/V({wave_shape}_{wave_freq}_{wave_cond}).csv", "rb"), delimiter=",")
-        assert mat_i.shape == mat_v.shape
+        assert mat_i.shape == mat_v.shape, f"Received two CSV files with different shapes - {mat_i.shape} and {mat_v.shape} for I|V({wave_shape}_{wave_freq}_{wave_cond}).csv"
         num_samples, sample_length = mat_v.shape
+        # assert sample_length == 8192, f"Expected sample length of 8192 but got {sample_length}"
 
         # Convert to long list of vectors
         # (num_samples, sample_length) -> (num_samples * sample_length, )
@@ -39,7 +41,6 @@ def preprocess(raw_data_dir="data/raw/", clean_data_dir="data/clean/"):
         long_vec_v = np.concatenate(vecs_v)
         assert len(long_vec_i) == num_samples * sample_length
         assert len(long_vec_v) == num_samples * sample_length
-
         # Stack vectors into a matrix
         long_vec_i = np.expand_dims(long_vec_i, axis=1)
         long_vec_v = np.expand_dims(long_vec_v, axis=1)
@@ -50,10 +51,21 @@ def preprocess(raw_data_dir="data/raw/", clean_data_dir="data/clean/"):
             f"{clean_data_dir}/data_{wave_shape}_{wave_freq}_{wave_cond}.csv",
             clean_mat,
             delimiter=",",
-            header="2 us,8192",
-            comments=''
+            header=f"2 us,{sample_length}",
+            comments='',
         )
+        data_info = [f"data_{wave_shape}_{wave_freq}_{wave_cond}.csv", wave_shape, wave_freq, wave_cond, num_samples, "2 us", sample_length]
+        data_infos.append(data_info)
 
+    data_infos = np.vstack(data_infos)
+    np.savetxt(
+        f"{clean_data_dir}/info.csv",
+        data_infos,
+        delimiter=",",
+        header="filename,shape,frequency,condition,num_samples,sample_period,sample_length",
+        comments="",
+        fmt="%s",
+    )
 
 if __name__ == "__main__":
     start_time = time.time()
