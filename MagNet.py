3635a3f7eb711830548cfbf0403cb9fbd7a1b248
Seungjae Ryan Lee <seungjaeryanlee@gmail.com>
2020-05-28 06:06:22 +0900

    Refactor MagNet.py

--- MagNet.py
+++ MagNet.py
@@ -1,86 +1,85 @@
-#%%========== Import Packages and Modules ====================================
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import pywt
-import csv
 from skimage.transform import resize
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
+
 #%%========== Import V-I Data ================================================
-datalength = 1000
-samplerate = 2e-6
+data_length = 1000
+sample_rate = 2e-6
 
-t = np.arange(0, (datalength-0.5)*samplerate, samplerate)
+t = np.arange(0, (data_length-0.5) * sample_rate, sample_rate)
 
-# I_sin_1k = pd.read_csv('D:\Dropbox (Princeton)\Raw Data_hiB\I(sin_1k_hiB).csv',header=None).iloc[:,1:datalength+1]
-# I_sin_2k = pd.read_csv('D:\Dropbox (Princeton)\Raw Data_hiB\I(sin_2k_hiB).csv',header=None).iloc[:,1:datalength+1]
-I_sin_5k = pd.read_csv('D:\Dropbox (Princeton)\Raw Data_hiB\I(sin_5k_hiB).csv',header=None).iloc[:,1:datalength+1]
-# V_sin_1k = pd.read_csv('D:\Dropbox (Princeton)\Raw Data_hiB\V(sin_1k_hiB).csv',header=None).iloc[:,1:datalength+1]
-# V_sin_2k = pd.read_csv('D:\Dropbox (Princeton)\Raw Data_hiB\V(sin_2k_hiB).csv',header=None).iloc[:,1:datalength+1]
-V_sin_5k = pd.read_csv('D:\Dropbox (Princeton)\Raw Data_hiB\V(sin_5k_hiB).csv',header=None).iloc[:,1:datalength+1]
+# I_sin_1k = pd.read_csv('data/raw/I(sin_1k_hiB).csv',header=None).iloc[:,1:data_length+1]
+# I_sin_2k = pd.read_csv('data/raw/I(sin_2k_hiB).csv',header=None).iloc[:,1:data_length+1]
+I_sin_5k = pd.read_csv('data/raw/I(sin_5k_hiB).csv',header=None).iloc[:,1:data_length+1]
+# V_sin_1k = pd.read_csv('data/raw/V(sin_1k_hiB).csv',header=None).iloc[:,1:data_length+1]
+# V_sin_2k = pd.read_csv('data/raw/V(sin_2k_hiB).csv',header=None).iloc[:,1:data_length+1]
+V_sin_5k = pd.read_csv('data/raw/V(sin_5k_hiB).csv',header=None).iloc[:,1:data_length+1]
 
-# I_tri_1k = pd.read_csv('D:\Dropbox (Princeton)\Raw Data_hiB\I(tri_1k_hiB).csv',header=None).iloc[:,1:datalength+1]
-# I_tri_2k = pd.read_csv('D:\Dropbox (Princeton)\Raw Data_hiB\I(tri_2k_hiB).csv',header=None).iloc[:,1:datalength+1]
-I_tri_5k = pd.read_csv('D:\Dropbox (Princeton)\Raw Data_hiB\I(tri_5k_hiB).csv',header=None).iloc[:,1:datalength+1]
-# V_tri_1k = pd.read_csv('D:\Dropbox (Princeton)\Raw Data_hiB\V(tri_1k_hiB).csv',header=None).iloc[:,1:datalength+1]
-# V_tri_2k = pd.read_csv('D:\Dropbox (Princeton)\Raw Data_hiB\V(tri_2k_hiB).csv',header=None).iloc[:,1:datalength+1]
-V_tri_5k = pd.read_csv('D:\Dropbox (Princeton)\Raw Data_hiB\V(tri_5k_hiB).csv',header=None).iloc[:,1:datalength+1]
+# I_tri_1k = pd.read_csv('data/raw/I(tri_1k_hiB).csv',header=None).iloc[:,1:data_length+1]
+# I_tri_2k = pd.read_csv('data/raw/I(tri_2k_hiB).csv',header=None).iloc[:,1:data_length+1]
+I_tri_5k = pd.read_csv('data/raw/I(tri_5k_hiB).csv',header=None).iloc[:,1:data_length+1]
+# V_tri_1k = pd.read_csv('data/raw/V(tri_1k_hiB).csv',header=None).iloc[:,1:data_length+1]
+# V_tri_2k = pd.read_csv('data/raw/V(tri_2k_hiB).csv',header=None).iloc[:,1:data_length+1]
+V_tri_5k = pd.read_csv('data/raw/V(tri_5k_hiB).csv',header=None).iloc[:,1:data_length+1]
 
-# I_trap_1k = pd.read_csv('D:\Dropbox (Princeton)\Raw Data_hiB\I(trap_1k_hiB).csv',header=None).iloc[:,1:datalength+1]
-# I_trap_2k = pd.read_csv('D:\Dropbox (Princeton)\Raw Data_hiB\I(trap_2k_hiB).csv',header=None).iloc[:,1:datalength+1]
-I_trap_5k = pd.read_csv('D:\Dropbox (Princeton)\Raw Data_hiB\I(trap_5k_hiB).csv',header=None).iloc[:,1:datalength+1]
-# V_trap_1k = pd.read_csv('D:\Dropbox (Princeton)\Raw Data_hiB\V(trap_1k_hiB).csv',header=None).iloc[:,1:datalength+1]
-# V_trap_2k = pd.read_csv('D:\Dropbox (Princeton)\Raw Data_hiB\V(trap_2k_hiB).csv',header=None).iloc[:,1:datalength+1]
-V_trap_5k = pd.read_csv('D:\Dropbox (Princeton)\Raw Data_hiB\V(trap_5k_hiB).csv',header=None).iloc[:,1:datalength+1]
+# I_trap_1k = pd.read_csv('data/raw/I(trap_1k_hiB).csv',header=None).iloc[:,1:data_length+1]
+# I_trap_2k = pd.read_csv('data/raw/I(trap_2k_hiB).csv',header=None).iloc[:,1:data_length+1]
+I_trap_5k = pd.read_csv('data/raw/I(trap_5k_hiB).csv',header=None).iloc[:,1:data_length+1]
+# V_trap_1k = pd.read_csv('data/raw/V(trap_1k_hiB).csv',header=None).iloc[:,1:data_length+1]
+# V_trap_2k = pd.read_csv('data/raw/V(trap_2k_hiB).csv',header=None).iloc[:,1:data_length+1]
+V_trap_5k = pd.read_csv('data/raw/V(trap_5k_hiB).csv',header=None).iloc[:,1:data_length+1]
 
-I = pd.concat([I_sin_5k , I_tri_5k , I_trap_5k] ,ignore_index=True)
-V = pd.concat([V_sin_5k , V_tri_5k , V_trap_5k] ,ignore_index=True)
+I = pd.concat([I_sin_5k , I_tri_5k , I_trap_5k], ignore_index=True)
+V = pd.concat([V_sin_5k , V_tri_5k , V_trap_5k], ignore_index=True)
 
 #%%========== Display Scalogram (uncomment when needed) ======================
-signal = V_trap_5k.iloc[100,1:1002]
+signal = V_trap_5k.iloc[100, 1:1002]
 
-wavename = 'cgau8'
-totalscale = 30
-fc = pywt.central_frequency(wavename)
+wave_name = 'cgau8'
+total_scale = 30
+fc = pywt.central_frequency(wave_name)
 fmax = 10e3
-cparam = (1/samplerate)/fmax * fc * totalscale
-scales = cparam / np.arange(totalscale, 1, -1)
-[cwtmatr, frequencies] = pywt.cwt(signal, scales, wavename, samplerate)
+cparam = (1 / sample_rate) / fmax * fc * total_scale
+scales = cparam / np.arange(total_scale, 1, -1)
+[cwtmatr, frequencies] = pywt.cwt(signal, scales, wave_name, sample_rate)
 
-plt.figure(figsize=(8, 4))
-plt.subplot(211)
-plt.contourf(t[1:1000], frequencies, abs(cwtmatr))
-plt.subplot(212)
-imagesize = 24
-plt.contourf(resize(abs(cwtmatr),(imagesize, imagesize)))
-plt.show()
+# plt.figure(figsize=(8, 4))
+# plt.subplot(211)
+# plt.contourf(t[1:1000], frequencies, abs(cwtmatr))
+# plt.subplot(212)
+# image_size = 24
+# plt.contourf(resize(abs(cwtmatr), (image_size, image_size)))
+# plt.show()
 
 #%%========== Compute Measured Loss ==========================================
 P = V * I
-datasize = np.size(P,0)
-Loss_meas = np.zeros(datasize)
+data_size = np.size(P, 0)
+Loss_meas = np.zeros(data_size)
 for index, row in P.iterrows():
-    Loss_meas[index] = np.trapz(row,t) / (samplerate*datalength)
-    
-#%%========== Create Scalograms ==============================================
-wavename = 'cgau8'
-totalscale = 30
-fc = pywt.central_frequency(wavename)
-fmax = 10e3
-cparam = (1/samplerate)/fmax * fc * totalscale
-scales = cparam / np.arange(totalscale, 1, -1)
+    Loss_meas[index] = np.trapz(row, t) / (sample_rate * data_length)
 
-imagesize = 24;
-scalogram = np.zeros([datasize, imagesize, imagesize])
+#%%========== Create Scalograms ==============================================
+wave_name = 'cgau8'
+total_scale = 30
+fc = pywt.central_frequency(wave_name)
+fmax = 10e3
+cparam = (1 / sample_rate) / fmax * fc * total_scale
+scales = cparam / np.arange(total_scale, 1, -1)
+
+image_size = 24
+scalogram = np.zeros([data_size, image_size, image_size])
 for index, row in V.iterrows():
-    [cwtmatr, frequencies] = pywt.cwt(row, scales, wavename, samplerate)
-    scalogram[index:,:,] = resize(abs(cwtmatr),(imagesize, imagesize))
-    print(index,' finished \n')
-    
+    [cwtmatr, frequencies] = pywt.cwt(row, scales, wave_name, sample_rate)
+    scalogram[index:, :, ] = resize(abs(cwtmatr), (image_size, image_size))
+    print(f"{index} finished")
+
 #%%========== Config the Neural Network ======================================
 # function to count number of parameters
 # def get_n_params(model):
@@ -89,12 +88,12 @@ for index, row in V.iterrows():
 #         np += p.nelement()
 #     return np
 
-input_size  = imagesize*imagesize   # images in NxN pixels
+input_size  = image_size * image_size   # images in NxN pixels
 output_size = 1      # 1 regression output
 
 Dataset = torch.utils.data.TensorDataset(torch.from_numpy(scalogram), torch.from_numpy(Loss_meas))
-train_size = int(0.7*datasize)
-test_size = datasize - train_size
+train_size = int(0.7 * data_size)
+test_size = data_size - train_size
 train_db, test_db = torch.utils.data.random_split(Dataset, [train_size, test_size])
 batch_size = 64
 train_loader = torch.utils.data.DataLoader(train_db, batch_size=batch_size, shuffle=True)
@@ -122,8 +121,8 @@ class Net(nn.Module):
         # x = self.drop_out(x)
         x = self.fc2(x)
         return x
-net = Net()
-net = net.double()
+
+net = Net().double()
 criterion = nn.MSELoss()
 optimizer = optim.Adam(net.parameters(), lr=0.001)
 
@@ -169,9 +168,9 @@ with torch.no_grad():
         y_pred[index] = net(inputs)
         y_meas[index] = labels
         
-plt.figure(figsize = (8,8))
-plt.subplot()
-plt.scatter(y_meas, y_pred)
-plt.plot(y_meas,y_meas,'k--')
-plt.grid(True)
-plt.show()
+# plt.figure(figsize = (8,8))
+# plt.subplot()
+# plt.scatter(y_meas, y_pred)
+# plt.plot(y_meas, y_meas, 'k--')
+# plt.grid(True)
+# plt.show()
