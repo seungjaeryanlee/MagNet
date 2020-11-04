8126e04f277763389ba2bec53cc13c48ad1f63f9
Seungjae Ryan Lee <seungjaeryanlee@gmail.com>
2020-11-04 22:31:19 +0000

    Unzip upon downloading dataset

--- magnet/download.py
+++ magnet/download.py
@@ -1,11 +1,39 @@
 import os
+import tarfile
 
 import gdown
 
 
-def download_dataset(download_path="data/"):
-    os.makedirs(download_path)
+def download_dataset(download_path: str = "data/"):
+    """
+    Download dataset from Google Drive
+
+    The dataset is a `magnet.tar.gz` file that is unzipped upon download. The
+    dataset only contains "clean", formatted data.
+
+    Parameters
+    ----------
+    download_path : str
+        The path to the folder to download the dataset to.
+    """
+    if not os.path.exists(download_path):
+        os.makedirs(download_path)
 
     url = "https://drive.google.com/uc?id=1fUfSH3odWmr5ORc3iZk3hPNbRr61f4Ls"
-    output = os.path.join(download_path, "magnet.tar.gz")
-    gdown.download(url, output, quiet=False)
+    zipped_file_path = os.path.join(download_path, "magnet.tar.gz")
+    gdown.download(url, zipped_file_path, quiet=False)
+    print("[MagNet] Dataset downloaded successfully.")
+
+    with tarfile.open(zipped_file_path, "r:gz") as tar:
+        tar.extractall(download_path)
+    print("[MagNet] Dataset unzipped successfully.")
+
+    os.remove(zipped_file_path)
+    print("[MagNet] Cleanup successful.")
+
+    print("███╗   ███╗ █████╗  ██████╗ ███╗   ██╗███████╗████████╗")
+    print("████╗ ████║██╔══██╗██╔════╝ ████╗  ██║██╔════╝╚══██╔══╝")
+    print("██╔████╔██║███████║██║  ███╗██╔██╗ ██║█████╗     ██║   ")
+    print("██║╚██╔╝██║██╔══██║██║   ██║██║╚██╗██║██╔══╝     ██║   ")
+    print("██║ ╚═╝ ██║██║  ██║╚██████╔╝██║ ╚████║███████╗   ██║   ")
+    print("╚═╝     ╚═╝╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚══════╝   ╚═╝   ")
