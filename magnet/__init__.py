79940d8132fe85aadcbc488bc0984e97c71a7b8a
Seungjae Ryan Lee <seungjaeryanlee@gmail.com>
2020-12-02 22:27:14 +0000

    Add PyTorchVoltageToCoreLossDataset

--- magnet/__init__.py
+++ magnet/__init__.py
@@ -1,6 +1,6 @@
 from .models import pytorch
 from .download import download_dataset
-from .pytorch_dataset import PyTorchDataset
+from .pytorch_dataset import PyTorchDataset, PyTorchVoltageToCoreLossDataset
 from .tensorflow_dataset import TensorFlowDataset
 
 
@@ -8,5 +8,6 @@ __all__ = [
     download_dataset,
     pytorch,
     PyTorchDataset,
+    PyTorchVoltageToCoreLossDataset,
     TensorFlowDataset,
 ]
--- magnet/pytorch_dataset.py
+++ magnet/pytorch_dataset.py
@@ -39,3 +39,45 @@ def PyTorchDataset(download_path: str = "data/", download=True):
     dataset = torch.utils.data.TensorDataset(voltage, current)
 
     return dataset
+
+
+# TODO(seungjaeryanlee): Find a better name
+def PyTorchVoltageToCoreLossDataset(download_path: str = "data/", download=True):
+    """
+    Return a PyTorch-style dataset for predicting core loss from voltage data.
+
+    Parameters
+    ----------
+    download_path : str
+        Path to the downloaded data
+    download : bool
+        If true, data is downloaded automatically if it does not exist.
+
+    Returns
+    -------
+    dataset : torch.utils.data.Dataset
+        PyTorch-style dataset for predicting core loss from voltage data
+    """
+    if download:
+        if not os.path.exists(os.path.join(download_path, "dataset.npy")):
+            download_dataset(download_path)
+    else:
+        if not os.path.exists(os.path.join(download_path, "dataset.npy")):
+            print("[MagNet] Dataset does not exist. Please call with download=True.")
+
+    # TODO(seungjaeryanlee): Add these as parameters to method
+    DATA_LENGTH = 400
+    SAMPLE_RATE = 2e-6
+
+    datas = np.load(os.path.join(download_path, "dataset.npy"))
+    voltage = torch.FloatTensor(datas[:, :DATA_LENGTH, 0])
+    current = torch.FloatTensor(datas[:, :DATA_LENGTH, 1])
+
+    power = voltage * current
+    t = np.arange(0, (DATA_LENGTH - 0.5) * SAMPLE_RATE, SAMPLE_RATE)
+    core_loss = np.trapz(power, t, axis=1) / (SAMPLE_RATE * DATA_LENGTH)
+    core_loss = torch.FloatTensor(core_loss).unsqueeze(1)
+
+    dataset = torch.utils.data.TensorDataset(voltage, core_loss)
+
+    return dataset
